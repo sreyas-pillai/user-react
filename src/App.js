38b35526f5d4d39686fd35cbad830fc0756120d3
sreyas-pillai <sreyas.pillai@bluepineapple.io>
2021-03-09 09:53:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Home from "./components/Home";
+import About from './components/About';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router,Route,Switch } from 'react-router-dom' ;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Router>
+     <div className="App">
+    <Navbar/>
+
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/about" component={About}/>
+      
+    </Switch>
+    
+    
+
     </div>
+   </Router> 
+    
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return (
+        <div className="container">
+            <div className="py-4">
+                <h1>Home Page</h1>
+                <table class="table border shadow">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Name</th>
+      <th scope="col">User Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+       <td>
+                  <Link class="btn btn-primary mr-2" >
+                    View
+                  </Link>
+                  <Link
+                    class="btn btn-outline-primary mr-2"
+                    
+                  >
+                    Edit
+                  </Link>
+                  <Link
+                    class="btn btn-danger"
+                    
+                  >
+                    Delete
+                  </Link>
+                </td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+
+            </div>
+        </div>
+    );
+
+}
+
+
+export default Home;
