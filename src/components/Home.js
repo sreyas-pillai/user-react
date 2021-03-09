4abfaa8bb6be2342edd1dde12993c595ef900973
sreyas-pillai <sreyas.pillai@bluepineapple.io>
2021-03-09 09:59:46 +0530

    new commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div className="container">
             <div className="py-4">
-                <h1>Home Page</h1>
+                <h1>Home Page User</h1>
                 <table class="table border shadow">
   <thead class="thead-dark">
     <tr>
